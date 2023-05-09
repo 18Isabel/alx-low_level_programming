0b5081bc35d228fb9e4f80a5c9b4d0e6ce431477
Isabel <18isabelbanda@gmail.com>
2023-05-09 13:45:57 -0700

    push task 0-9

--- 0x07-pointers_arrays_string/main.h
+++ 0x07-pointers_arrays_string/main.h
@@ -1,20 +1,14 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
-  *_memset - fills meory with a constant byte
-  *@s: memory area to be filled
-  *@b: character to copy
-  *@n: number of times to copy b
-  *
-  *Return: pointer to the memory area s
-  */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int x;
-
-	for (x = 0; x < n; x++)
-	{
-		s[x] = b;
-	}
-	return (s);
-}
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+#endif
